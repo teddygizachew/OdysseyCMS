227a14e0c0c62fde401c4b17ef5325caf81c0202
Lulseged Admasu <lulsegedgirma10@gmail.com>
2023-03-11 02:21:25 -0500

    events page completed. edit/create/add/detail/index pages are working!

--- entities/events/CRUD/app.js
+++ entities/events/CRUD/app.js
@@ -2,14 +2,16 @@ const events={
 	documentID:'1083937811262095360',
 	index:function(){
 		let events_list = document.querySelector('.events__list')
-		events_list.innerHTML='Loading quotes, please wait...';
+		events_list.innerHTML='<div class="card">Loading quotes, please wait...</div> ';
 		events_database.index(events.documentID,function(items){
 			events_list.innerHTML='';
 			for(let i=0;i<items.length;i++){
 				let event=items[i];
 				events_list.innerHTML += `
-					<a class="card" href=CRUD/detail.html?event=${i}>
+					<a class="card event__card" href=CRUD/detail.html?event=${i}>
 						<h4>${event.name}</h4>
+						<p><i class="fa-solid fa-calendar-day"></i>${event.date}</p>
+						<p><i class="fa-solid fa-location-dot"></i>${event.location}</p>
 					</a>
 				`;
 			}
@@ -45,17 +47,15 @@ const events={
 			let keywords=document.querySelector('form textarea[name=keywords]');
 			let org=document.querySelector('form input[name=org]');
 
-			// get attendees
 			// generate id for event
 			let id = Math.floor(Math.random() * 54215);
-			console.log(id)
 			let newEvent={
 				id: `e-${id}`,
 				name: name.value,
 				location: location.value,
 				date: date.value,
 				time: time.value,
-				keywords: keywords.value.split(','),
+				keywords: keywords.value.trim().split(','),
 				description: description.value,
 				org: org.value,
 			}
@@ -64,20 +64,40 @@ const events={
 		});
 	},
 	update:function(index){
-		database.detail(quotes.documentID,index,function(item){
-			document.getElementById('loading').style.display='none';
-			document.querySelector('form input[name=author]').value=item.author;
-			document.querySelector('form textarea[name=quote]').value=item.quote;
+		events_database.detail(events.documentID,index,function(item){
+			document.querySelector('form input[name=name]').value=item.name
+			document.querySelector('form input[name=location]').value=item.location
+			document.querySelector('form input[name=date]').value=item.date
+			document.querySelector('form input[name=time]').value=item.time
+			document.querySelector('form textarea[name=description]').value=item.description
+			document.querySelector('form textarea[name=keywords]').value=item.keywords
+			document.querySelector('form input[name=org]').value=item.organizer
+
+	
 			
 			document.querySelector('form').addEventListener('submit',function(e){
 				e.preventDefault();
-				let author=document.querySelector('form input[name=author]');
-				let quote=document.querySelector('form textarea[name=quote]');
-				let newQuote={
-					author:author.value,
-					quote:quote.value
+				let name=document.querySelector('form input[name=name]');
+				let location=document.querySelector('form input[name=location]');
+				let date=document.querySelector('form input[name=date]');
+				let time=document.querySelector('form input[name=time]');
+				let description=document.querySelector('form textarea[name=description]');
+				let keywords=document.querySelector('form textarea[name=keywords]');
+				let org=document.querySelector('form input[name=org]');
+
+				// generate id for event
+				let id = Math.floor(Math.random() * 854215);
+				let newEvent={
+					id: `e-${id}`,
+					name: name.value,
+					location: location.value,
+					date: date.value,
+					time: time.value,
+					keywords: keywords.value.split(','),
+					description: description.value,
+					org: org.value,
 				}
-				database.update(quotes.documentID,index,newQuote);
+				events_database.update(events.documentID, index, newEvent)
 			});
 		});
 	}
